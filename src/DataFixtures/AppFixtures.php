edcb14ca693dc9690ba05bad2cda99a805d9e71f
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-06-04 12:47:42 +0200

    feat(fixtures) Ajout de fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Conseil;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $conseil = new Conseil();
+            $conseil->setContent('Conseil numéro ' . $i . ' : Pensez à arroser vos plantes régulièrement.');
+            $conseil->setMois(rand(1, 12));
+
+            $manager->persist($conseil);
+        }
 
         $manager->flush();
     }
