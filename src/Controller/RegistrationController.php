1eb86f2c90e8c7cf8b3b67b8eaf49096bffd9e3e
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-06-11 15:45:04 +0200

    feat(user) Mise en place de l'édition d'un utilisateur

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -13,7 +13,9 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Nelmio\ApiDocBundle\Attribute\Model;
 use OpenApi\Attributes as OA;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\Exception\NotNormalizableValueException;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 final class RegistrationController extends AbstractController
 {
@@ -74,5 +76,56 @@ final class RegistrationController extends AbstractController
         $jsonUser = $serializer->serialize($user, 'json', ['groups' => ['read']]);
 
         return new JsonResponse($jsonUser, JsonResponse::HTTP_CREATED, [], true);
+    }
+
+    #[Route("/api/user/{id}", name: "update_user", methods: ["PUT"])]
+    #[IsGranted("ROLE_ADMIN", message: "Vous n'avez pas les droits suffisants")]
+    #[OA\Parameter(
+        name: "id",
+        in: "path",
+        required: true,
+        description: "L'id de l'utilisateur à mettre à jour",
+        schema: new OA\Schema(type: "integer")
+    )]
+    #[OA\RequestBody(
+        required: true,
+        description: "Données de l'utilisateur à mettre à jour",
+        content: new OA\JsonContent(
+            ref: new Model(type: User::class, groups: ["write"])
+        )
+    )]
+    #[OA\Response(
+        response: 204,
+        description: "Utilisateur mis à jour avec succès"
+    )]
+    #[OA\Tag(name: "utilisateur")]
+    public function updateUser(
+        Request $request,
+        SerializerInterface $serializer,
+        User $currentUser,
+        EntityManagerInterface $em,
+        ValidatorInterface $validator
+    ) : JsonResponse 
+    {
+        try {
+            $serializer->deserialize(
+                $request->getContent(),
+                User::class,
+                "json",
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $currentUser]
+            );
+        } catch (NotNormalizableValueException $e) {
+            return new JsonResponse(['error' => 'Donnée invalide : ' . $e->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $errors = $validator->validate($currentUser);
+        if (count($errors) > 0) {
+            return new JsonResponse($serializer->serialize($errors, "json"), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }    
+        
+        $em->persist($currentUser);
+        $em->flush();
+        
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
